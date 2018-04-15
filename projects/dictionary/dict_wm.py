e01ed00f66245ed305833bed0e5f5fc6eddaa47f
Yong <neolee7300@gmail.com>
2018-04-14 22:54:23 -0400

    use gzip to store data

--- projects/dictionary/dict_wm.py
+++ projects/dictionary/dict_wm.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-  
 import sys, os, re 
 from bs4 import BeautifulSoup
-# For Python 3.0 and later
 from urllib.request import urlopen
 from importlib import reload
 # pickle save objects
-import pickle
+import pickle,gzip
 
 class Word_grabber:
     dict_web = 'https://www.merriam-webster.com/dictionary/'
@@ -28,7 +27,7 @@ class Word_grabber:
         #text = " ".join([s for s in text.splitlines() if s])   # join lines
         #text = " ".join(text.split()) # remove multiple spaces 
         self.output_string += (text +'\n') 
-        print(text)
+        #print(text)
 
     def show_objs(self, objs):
         for idx,obj in enumerate(objs) :
@@ -41,18 +40,20 @@ class Word_grabber:
 
         print('trying to get db from url --' + url + '\n')
         if os.path.isfile(url): 
-            with open(url, 'rb') as f:
+            with gzip.open(url, 'rb') as f:
                 self.dict_db = pickle.load(f)
                 #return pickle.load(f)
+            return self.dict_db
         else:
             print('url --' + url + ' is not there. \n')
+            return {}
 
 
     def save_db(self, url=None):
         # set default value in class memthod
         url = url if url is not None else self.db_url 
         print('trying to save db to url --' + url + '\n')
-        with open(url , 'wb') as f:
+        with gzip.open(url , 'wb') as f:
             pickle.dump(self.dict_db, f, 0)
             #pickle.dump(self.dict_db, f, pickle.HIGHEST_PROTOCOL)
 
@@ -92,17 +93,20 @@ class Word_grabber:
             print(' \nTrying to add word into db')
             self.dict_db.update({word_lookup:{'contents':self.output_string,'times':0}})
             print(' Word is in db')
-
+    
             self.update_db(word_lookup)
             # clean the output_string for next word
             self.output_string = ''
+            return 'found'
     
         except AttributeError:
             print( "word can not be found")
-            sys.exit(2)
+            return 'not found'
+            #sys.exit(2)
     
         except TypeError:
             print ("NA")
+            return 'wrong type'
 
 def main():
     reload(sys)                         # 2
@@ -123,6 +127,7 @@ def main():
     else:
         wg.grab_word_from_url(word_lookup)
         wg.save_db()
+        print(wg.dict_db[word_lookup]['contents'])
 
 if __name__ == '__main__':
     main()
