56774a009a2112c9b2f8b09c6204680a0de8aeec
Neo Lee <neolee7300@gmail.com>
2018-04-15 13:46:13 -0400

    start to prepare dictionary database

---
+++ projects/dictionary/create_dict.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-  
+import sys, os, re 
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from importlib import reload
+# pickle save objects
+import pickle,gzip,json
+
+import read_english_dictionary 
+import dict_wm 
+
+import random
+from time import sleep
+
+def create_new_words(english_words):
+
+    wg = dict_wm.Word_grabber('')
+    new_words={}
+    new_words=wg.load_db('./new_words.dict')
+    
+    random_keys=list(english_words.keys())    
+    random.shuffle(random_keys)
+
+    for key in random_keys:
+        new_words.update({key:{'contents':'','times':-1}})
+        
+    wg.dict_db=new_words    
+    wg.save_db('./new_words.dict')
+      
+    
+
+if __name__ == '__main__':
+    english_words = read_english_dictionary.load_words()
+    # demo print
+    # for idx,obj in enumerate(english_words) :
+    #    print ( str(idx) + 'th obj is ' + obj )
+    # create_new_words(english_words)
+
+    wg = dict_wm.Word_grabber('')
+
+    new_words=wg.load_db('./new_words.dict')
+    wrong_words=wg.load_db('./wrong_words.dict')
+    wg.dict_db = wg.load_db('./dict_db.dict')    
+
+    print('there are ' + str(len(new_words)) + ' words' )
+    while new_words :
+        for x in range(1, 200) :
+            word = new_words.popitem()[0]
+            print('checking ' + word + '  ' +str(len(new_words)) + ' words left')
+            print('left %d , right %d, wrong %d' % (len(new_words), len(wg.dict_db), len(wrong_words)))                         
+            #sleep(random.randint(1,2))
+            try:
+                result = wg.grab_word_from_url(word)
+                #print('checked ' + word + '  ' +str(len(new_words)) + ' words left')
+            except:
+                wrong_words.update({word:{'contents':'','times':-1}})
+                pass
+            
+        print('saving' + str(len(wg.dict_db)) + " good words ");
+        good_words = wg.dict_db;
+        wg.save_db('./dict_db.dict')        
+
+        wg.dict_db=wrong_words
+        #print(wrong_words.keys())
+        print('saving' + str(len(wg.dict_db)) + " wrong words ");
+        wg.save_db('./wrong_words.dict')    
+        
+        wg.dict_db=new_words    
+        print('saving' + str(len(wg.dict_db)) + " new words ");
+        wg.save_db('./new_words.dict')
+        wg.dict_db=good_words;        
+          
