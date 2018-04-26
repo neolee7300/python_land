e90ef668581a62433691b527c9d8729e1538a53d
Neo Lee <neolee7300@gmail.com>
2018-04-26 15:03:36 -0400

    cross operator generator is done.

--- projects/Rubiks_cube/rubiks.py
+++ projects/Rubiks_cube/rubiks.py
@@ -5,24 +5,51 @@ from importlib import reload
 # pickle save objects
 import pickle,gzip
 
-# Aux functions
-def cshift_list(seq, n):
-    n = n % len(seq)
-    return seq[n:] + seq[:n]
-
 class op_generator:
-    
-    def ijk_cross_rules(self, inputs):   # inputs as tuple? 
-        # rule1 :  i j k are rotating invariant 
-        # i:0 , j :1 , k:2   ,   
-        shift = inputs[0]
-        p1, p2 =  0, inputs[1] - shift 
-        # rule2  i x i = 0  i x j = k   i x k = -j
-        rule2 = {(0, 0): [0,0,0] , (0, 1): [0,0,1]  , (0, 2 ) : [0,-1,0]} 
-        return cshift_list(rule2[(p1,p2)] , -shift) 
+
+    ijk_to_vec = {'i': [1,0,0],  'j': [0,1,0],  'k': [0,0,1]} 
+    vec_to_ijk = { (1,0,0):'i' ,   (0,1,0):'j', (0,0,1):'k'} 
+
+    def rotate_input(self, input, n):
+        result = ()
+        print(input)
+        for ijk in input:
+            vec = self.cshift_list(self.ijk_to_vec[ijk], n)
+            result += tuple(self.vec_to_ijk[tuple(vec)])
+        return result
+
+    def flip_input(self, input):
+        return (input[1], input[0]) 
+
+    def ijk_cross_rules(self):   # inputs as tuple? 
+        # rule0  i x i = 0  i x j = k   i x k = -j
+        rule0 = {('i', 'i'): [0,0,0] , ('i', 'j'): [0,0,1] , ('i','k') : [0,-1,0]} 
+
+        # rule1  rotate i, j k, results are the same in rotated ijk  
+        for n in range(1,3) :
+            rotate = {self.rotate_input(k,n) : self.cshift_list(v,n) for k,v in rule0.items()}
+            rule0.update(rotate)
+
+        # rule2 flip input, the result is negative 
+        flip = {self.flip_input(k) : self.neg_vec(v) for k,v in rule0.items()}
+        rule0.update(flip)
+
+        return rule0 
+
+    # Aux functions
+    def cshift_list(self, seq, n):
+        n = n % len(seq)
+        return seq[n:] + seq[:n]
+
+    def neg_vec(self, vec):
+        return [ -v for v in vec ]
 
 def main():
     reload(sys)                         # 2
+    opg = op_generator()
+    cross_rules = opg.ijk_cross_rules()
+    print(cross_rules)
+    print(len(cross_rules))
 
 if __name__ == '__main__':
     main()
