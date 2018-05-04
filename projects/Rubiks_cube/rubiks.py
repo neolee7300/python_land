eda6fda9adf11c1dafc6631bdc011f2c88ac9625
Neo Lee <neolee7300@gmail.com>
2018-05-04 04:22:35 -0400

    expand to nth rank rubik

--- projects/Rubiks_cube/rubiks.py
+++ projects/Rubiks_cube/rubiks.py
@@ -4,64 +4,78 @@ import sys, os, re
 from importlib import reload
 # pickle save objects
 import pickle,gzip
-import numpy as np
 from itertools import * 
 
 class rubik3 :
-    color = cycle( ('r','g','b','w','y','p') )
+    color = cycle('rgbwyp')
 
     def __init__(self) :
-        a = np.array([1,0,0])  
-        b = np.array([0,1,0])  
-        acb = np.cross(a,b)
 
-        # position in cubic - xyz faces
-        self.coods = tuple(product([-1, 0, 1],[-1, 0, 1],[-1, 0, 1])) 
-        self.coods_xyz = tuple(product(self.coods, [0,1,2])) 
+        self.n = 1 
+        self.span =[x for x in range(-self.n, self.n + 1)] 
 
-        # xyz faces - position in face
-        self.faces = tuple(product([-1, 1],[0, 1, 2 ])) # (pos-neg , xyz )
+        # position in cubic - axies 
+        self.coods = tuple(cood for cood in product(self.span, self.span, self.span)
+                                              if -self.n in cood or self.n in cood) 
 
-        self.moves= tuple(product([-1, 1],[0, 1, 2 ],[90,180,270])) 
+        # tool for rotating face, no guarantee what's in it. 
+        self.rubiks = { cood: ['no_id','no_id','no_id'] for cood in self.coods}
 
-        self.rubiks = tuple() 
+        # face - position in current face
+        self.faces =tuple(x for x in product([-self.n , self.n],[0, 1, 2 ])) # (pos-neg , axies)
+        self.face_color = { face : next(self.color) for face in self.faces}
 
-        # the operator we want is a mapping of 
-        # face_yz + moves -> face_yz_new 
-        # rubic_new[face_yz] = operator= dict{(face_yz_new, move) : face_yz_old}
+        yz= tuple(x for x in product(self.span, self.span)) # (pos in face)
+        
+        self.faces_yz= tuple(face_yz for face_yz in product(self.faces, yz))# (faces, (y,z)) index is id
+        self.cood_axis = ()  # (cood,axis) index is id 
+        self.map_cood_axis_id()
 
-        self.create_mapping()
+        self.moves= tuple(x for x in product(self.faces, [90,180,270])) 
+        self.mapping = {} # {move : mapping = (new_id) } index of new_id is old_id  
+        self.create_mapping() #
+
         self.init_rubiks()
         self.rubiks_to_status()
         print(self.rubiks)
     
     def init_rubiks (self) :
-        for face in self.faces:
+        for face in self.faces : 
             self.pick_faces(face)
             self.paint_rubiks() 
             self.unpick_faces(face)
 
+    # grid is (cood, gn) = face,y,z is an object with (id, color, ... ),  
+    def map_cood_axis_id (self):
+        print(self.faces_yz)
+        for id,(face,(y,z)) in enumerate(self.faces_yz) :
+            self.pick_faces(face)
+            cood, axis = (self.n,y,z), face[1] 
+            self.rubiks = {cood :(id,-1,-1)} 
+            self.unpick_faces(face)
+            self.cood_axis += tuple((cood,axis) for cood in self.rubiks.keys())
+ 
     def rubiks_to_status (self) :
-        self.status = tuple(self.rubiks[cood][xyz]  for cood, xyz in self.coods_xyz)
+        self.status = tuple(self.rubiks[cood][axis] for cood, axis in self.cood_axis)
 
     def status_to_rubiks(self) :
         for cood in self.coods : 
-            v = tuple(self.status[self.coods_xyz.index((cood,n))] for n in range(3))
-            self.rubiks.update({cood:v })
+            v = tuple(self.status[self.cood_axis.index((cood,n))] for n in range(3))
+            self.rubiks.update({cood:v})
 
     def create_mapping (self):
         #  { data o : data o} -> {data n : data o}  ->  { id n : id o }
-        self.mapping =() 
+        # store the index structure direct in value    value[index_struct] = index_struct
+        self.mapping = {}
         for move in self.moves:
             self.rubiks = {cood: ((cood,0), (cood,1),(cood,2)) for cood in self.coods}
             self.rubiks_move(move)
-            move_mapping= tuple(self.rubiks[cood][xyz]  for cood, xyz in self.coods_xyz) 
-            self.mapping += (tuple(map(self.coods_xyz.index, move_mapping)),)
+            move_mapping= tuple(self.rubiks[cood][axis] for cood,axis in self.cood_axis) 
+
+            self.mapping.update({move:tuple(map(self.cood_axis.index, move_mapping))})
         
-    def map_move(self, move_id):
-        mapping = self.mapping[move_id]
-        self.status = [self.status[mapping[cid]] for cid in
-                       range(len(self.coods_xyz))]
+    def map_move(self, move):
+        self.status = [self.status[nid] for nid in self.mapping[move]]
     # This is not the most efficient way to twist a face. But it is how we
     # think in brain. The efficiency should be left for compiler to consider
     # All rubik moves could be composed by flip, select axies, rotate the
@@ -69,22 +83,21 @@ class rubik3 :
     # perspective for next move. 
     # move = ( pos or nag ,  xyz ,  angle/90 )  In total 2 * 3 * 3 = 18 moves 
     def rubiks_move(self, move):
-        face =(move[0],move[1])
+        face = move[0]
         self.pick_faces(face)
-        for times in range(int(move[2]/90)):
+        for times in range(int(move[1]/90)):
             self.rotate_1yz_90()
         self.unpick_faces(face)
 
     def pick_faces(self,face):
         self.rotate_xyz_rubiks(face[1]) 
-        if face[0] == -1: 
+        if face[0] < 0 : 
             self.flip_rubiks() 
 
     def unpick_faces(self,face):
-        if face[0] == -1:
+        if face[0] < 0 :
             self.flip_rubiks() 
         self.rotate_xyz_rubiks(3 - face[1] ) 
-
 
     def flip_rubiks (self):
         self.rubiks = { (-k[0],) + k[1:] : v for k,v in self.rubiks.items()}
@@ -94,17 +107,18 @@ class rubik3 :
 
     def rotate_1yz_90(self):
         return self.rubiks.update({(k[0], -k[2], k[1]): (v[0],v[2],v[1]) for k,v in
-                      self.rubiks.items() if k[0] == 1})
+                      self.rubiks.items() if k[0] == self.n})
 
     def paint_rubiks (self):
         co = next(self.color)
-        self.rubiks.update({k :((co,) + v[1:]) for k,v in self.rubiks.items() if k[0] == 1})
+        self.rubiks.update({k :((co,) + v[1:]) for k,v in self.rubiks.items()
+                            if k[0] == self.n})
 
     def show_faces(self,face):
         print(face)
         self.pick_faces(face)
-        for z in [-1, 0, 1] : 
-            print( self.rubiks[(1,-1,z)][0], self.rubiks[(1,0,z)][0], self.rubiks[(1,1,z)][0]) 
+        for z in self.span :
+            print( [self.rubiks[(self.n, y, z)][0] for y in self.span] )
         self.unpick_faces(face)
 
     def show_rubiks(self):
